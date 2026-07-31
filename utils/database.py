af646860cc84cb733daab84453cf9571a57d449b
boddugideon <gideonboddu@gmail.com>
2026-07-31 17:22:06 +0530

    Add cloud database port

--- utils/database.py
+++ utils/database.py
@@ -8,9 +8,11 @@ load_dotenv()
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
+    "port": int(os.getenv("DB_PORT", "3306")),
     "user": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD"),
     "database": os.getenv("DB_NAME", "medintel_ai"),
+    "connection_timeout": 15,
 }
 
 
